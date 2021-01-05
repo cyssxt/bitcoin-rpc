f6e9417fc1b3381339f270b0f3ff62a15e08b99e
徐涛 <cyssxt@163.com>
2021-01-05 19:36:10 +0800

    Initial commit

---
+++ src/main/java/WalletJava.java
@@ -0,0 +1,90 @@
+import com.google.gson.Gson;
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
+import okhttp3.*;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+public class WalletJava {
+
+    private final static String URL ="http://localhost:19001";
+
+    private final static String USER = "admin1";
+    private final static String PASS = "123";
+
+    public static void main(String[] args) throws IOException {
+//        System.out.println(getNewAddress("cccc"));
+        System.out.println(getReceivedByAddress("2NB84S5tttA9df3y5ay983bpqkXZsfwo7gs"));
+    }
+
+    public static String getAuthorization(String user,String pass){
+        String auth = String.format("%s:%s",user,pass);
+        System.out.println(auth);
+        return String.format("Basic %s", Base64.encode(auth.getBytes(StandardCharsets.UTF_8)));
+    }
+
+    public static String getAuthorization(){
+        return getAuthorization(USER,PASS);
+    }
+
+    //获取新地址
+    /**
+     *
+     * @param label//获取新地址
+     */
+    public static String getNewAddress(String label) throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, "{\n    \"method\":\"getnewaddress\",\"params\":[\""+label+"\"]\n}");
+        Response response = client.newCall(builder(body)).execute();
+        Gson gson = new Gson();
+        Map<String,String> map = gson.fromJson(response.body().string(), Map.class);
+        return map.get("result");
+    }
+
+    //转账
+    public static void sendToAddress(String address, BigDecimal num,String remark,String personName) throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, "{\n    \"jsonrpc\": \"1.0\",\n    \"id\": \"curltest\",\n    \"method\": \"sendtoaddress\",\n    \"params\": [\n        \""+address+"\",\n        "+num+",\n        \""+remark+"\",\n        \""+personName+"\"\n    ]\n}");
+        Response response = client.newCall(builder(body)).execute();
+    }
+
+    public static Request builder(RequestBody body){
+        String authorization = getAuthorization();
+        System.out.println(authorization);
+        return new Request.Builder()
+                .url(URL)
+                .method("POST", body)
+                .addHeader("Authorization", authorization)
+                .addHeader("Content-Type", "application/json")
+                .build();
+    }
+
+    //获取钱包余额
+    public static Object getBalance() throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, "{\n    \"method\":\"getbalance\"\n}");
+        Response response = client.newCall(builder(body)).execute();
+        Gson gson = new Gson();
+        Map<String,Object> map = gson.fromJson(response.body().string(), Map.class);
+        return map.get("result");
+    }
+
+    //获取钱包余额
+    public static Object getReceivedByAddress(String address) throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, "{\"method\":\"getreceivedbyaddress\",\"params\":[\""+address+"\"]}");
+        Response response = client.newCall(builder(body)).execute();
+        Gson gson = new Gson();
+        Map<String,Object> map = gson.fromJson(response.body().string(), Map.class);
+        return map.get("result");
+    }
+}
